898543340f5ed9218344ac45657f1d33e4534b67
slowbackspace <slowbackspace@gmail.com>
2018-11-26 13:26:11 +0100

    allow request from all domains

--- api/middleware/cors.js
+++ api/middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://angry-leakey-e80d2d.netlify.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
